f41efc3b7f2e1aab8221ac0c4b70415e8a109d89
Dao Manh Thang <thangdm1@vnpt.vn>
2024-03-15 07:51:27 +0700

    Chuyen sang dung gtts

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, Request, HTTPException
 from gtts import gTTS
-import pyttsx3
+import pyttsx4
 import os
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/download", StaticFiles(directory="download"), name="download")
 
 @app.post("/tts")
 async def tts(request: Request):
@@ -10,26 +14,40 @@ async def tts(request: Request):
     text = data["text"]
     lang = data.get("lang", "vi")
     speed = data.get("speed", 1.0)
-
+    slow = data.get("slow", False)
+    model = data.get("model", "pytts")
+    filename = data.get("file_name", "output.mp3")
     try:
-        # Tạo đối tượng TTS
-        engine = pyttsx3.init()
-        voices = engine.getProperty('voices')
-        engine.setProperty('voice', voices[1].id)
+        if model == "gtts":
+            tts = gTTS(text=text, lang=lang, slow=slow)
+            tts.save(filename)
+        else:
+            # Tạo đối tượng TTS
+            engine = pyttsx4.init()
+            voices = engine.getProperty('voices')
+            engine.setProperty('voice', data.get("voice", voices[1].id))
 
-        # Đọc văn bản
-        #engine.say(text)
+            # engine.setProperty('volume', volume-0.25)
+            engine.setProperty('rate', data.get("rate", 50))
 
-        # Lưu file MP3
-        engine.save_to_file(text, "output.mp3")
-        engine.runAndWait()
-        os.rename("output.mp3", "/app/output1.mp3")
+            # Đọc văn bản
+            #engine.say(text)
+            # Lưu file MP3
+            engine.save_to_file(text, filename)
+            engine.runAndWait()
+        if speed == 1.0:
+            os.rename(filename, "/app/download/"+filename)
+        else:
+            from pydub import AudioSegment
+            audio = AudioSegment.from_mp3(filename)
+            final = audio.speedup(playback_speed=speed)
+            final.export("/app/download/"+filename, format="mp3")
 
-        return {"success": True, "file_name": "output.mp3"}
+        return {"success": True, "file_name": filename}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/voices")
 async def voices():
-    voices = pyttsx3.init().getProperty('voices')
-    return {"voices": [{"name": voice.name, "id": voice.id, "language": voice.languages} for voice in voices]}
+    voices = pyttsx4.init().getProperty('voices')
+    return {"voices": [{"name": voice.name, "id": voice.id, "language": voice.languages, "voice":voice} for voice in voices]}
